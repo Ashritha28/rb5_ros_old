bfa9c0aab37a933e56b40292b499a1f9f14929ba
Sai Ashritha Kandiraju <saiashrithakandiraju@Sais-MacBook-Pro-2.local>
2022-10-23 11:59:28 -0700

    Added transformation to get current state

--- rb5_control/src/localization_node.py
+++ rb5_control/src/localization_node.py
@@ -8,61 +8,51 @@ from april_detection.msg import AprilTagDetectionArray, AprilTagDetection
 import numpy as np
 import time
 import tf
+from scipy.spatial.transform import Rotation
 
 #pose_pub = rospy.Publisher('/current_pose', Pose, queue_size=1)
 
 # Location of the marker AprilTag
-pose_ma = {4: np.asarray([[1, 0, 0, 0],[0, 1, 0, 0], [0,0,1, 1.7], [0,0,0,1]]),
-            0: np.asarray([[1, 0, 0, 0],[0, 1, 0, 0], [0,0,1, 2.8], [0,0,0,1]]),
-            12: np.asarray([[1, 0, 0, 0],[0, 1, 0, 0], [0,0,1, 2.8], [0,0,0,1]])}
+pose_ma = {8: np.asarray([[0, -1, 0, 2.05],[0, 0, -1, 0.015], [1, 0, 0, 0.15], [0,0,0,1]])}
 
 # Camera in robot frame
-rTc = np.asarray([[-1, 0, 0, 0], [0, -1, 0, 0], [0,0,1, -0.10], [0,0,0,1]])
+rTc = np.asarray([[0, -1, 0, 0.05], [0, 0, -1, 0.015], [0,0,1, 0.15], [0,0,0,1]])
 
 def tag_callback(msg):
 
     for detection in msg.detections:
-        #print("Type:",type(detection))
-        print("ID:", detection.id)
-        print("Pose:", detection.pose)
-        tag_orientation = detection.pose.orientation 
-        print("X quaternion:", tag_orientation.x)
-        # print("Rotation Matrix:", tf.transformations.quaternion_matrix([tag_orientation.x, tag_orientation.y, 
-        # tag_orientation.z, tag_orientation.w]))
-        print("Rotation Matrix:", tf.transformations.euler_from_quaternion([tag_orientation.x, tag_orientation.y, 
-        tag_orientation.z, tag_orientation.w]))
 
+        #print("Type:",type(detection))
+        apriltag_id = detection.id
+        position = np.array(detection.pose.position)
+        print("ID:", apriltag_id)
+        print("Position:", position)
+        tag_orientation = detection.pose.orientation 
+        print("Tag oreintation quaternion:", tag_orientation)
+        
+        r = np.array(Rotation.from_quat(tag_orientation))
+        print("Rotation matrix using scipy:", r)
+        print("Rotation Matrix using tf: \n", tf.transformations.quaternion_matrix([tag_orientation.x, tag_orientation.y, 
+        tag_orientation.z, tag_orientation.w]))
+        cTa = (np.concatenate((np.concantenate((r, position), axis=1),np.array([0,0,0,1])),axis=0)
+        print("cTa: \n",cTa)
+        aTc = np.linalg.inv(cTa)
+        # AprilTag in robot coordinates
+        rTa = np.matmul(aTc, rTc)
+        print("AprilTag in robot coordinates rTa: \n",rTa)
+        # Robot in world coordinates
+        wTr = np.matmul(pose_ma[apriltag_id], rTa)
+        print("Robot in world coordinates wTr: \n",wTr)
+        # new[apriltag_id] = wTr
+        # pose_msg.pose.matrix = list(wTr[:3, :].flatten())
+
+    
         # print("ID:",msg.id,"\n Pose:",msg.pose)
         # for id, pose in zip(msg.id, msg.pose) :
             # print("april_tag detetcion:", detection)
-
-    # pose_msg = Pose()
-    # pose_msg.header.stamp = msg.header.stamp
-    # robot_r = rospy.Rate(5)
-    # if len(msg.detections):
-    #     new = {}
-    #     for apriltag_id, pose in zip(msg.ids, msg.detections):
-    #             # Location of AprilTag in camera coordinates
-    #             cTa = np.asarray(list(pose.matrix)).reshape((4,4))
-    #             # Inverse of cTa - Location of camera with respect to april tag
-    #             aTc = np.linalg.inv(cTa)
-    #             # AprilTag in robot coordinates
-    #             rTa = np.matmul(aTc, rTc)
-    #             # Robot in world coordinates
-    #             wTr = np.matmul(pose_ma[4], rTa)
-    #             new[apriltag_id] = wTr
-    #             pose_msg.pose.matrix = list(wTr[:3, :].flatten())
-    #             pose_pub.publish(pose_msg)
-    # robot_r.sleep()
-
-# def tag_callback1(msg):
-#     if len(msg.)
-
 
 
 if __name__ == "__main__":
     rospy.init_node('localization_node')
     rospy.Subscriber("/apriltag_detection_array", AprilTagDetectionArray, tag_callback)
-    # rospy.
-    # rospy.Subscriber("/april_poses",AprilTagDetection, tag_callback )
     rospy.spin()
