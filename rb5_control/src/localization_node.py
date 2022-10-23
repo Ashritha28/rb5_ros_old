b597268a2c3f09bb3acd65f1820f7c106774880a
Sai Ashritha Kandiraju <saiashrithakandiraju@Sais-MacBook-Pro-2.local>
2022-10-22 19:42:16 -0700

    Modified localization node

--- rb5_control/src/localization_node.py
+++ rb5_control/src/localization_node.py
@@ -27,7 +27,9 @@ def tag_callback(msg):
         print("Pose:", detection.pose)
         tag_orientation = detection.pose.orientation 
         print("X quaternion:", tag_orientation.x)
-        print("Rotation Matrix:", tf.transformations.quaternion_matrix([tag_orientation.x, tag_orientation.y, 
+        # print("Rotation Matrix:", tf.transformations.quaternion_matrix([tag_orientation.x, tag_orientation.y, 
+        # tag_orientation.z, tag_orientation.w]))
+        print("Rotation Matrix:", tf.transformations.euler_from_quaternion([tag_orientation.x, tag_orientation.y, 
         tag_orientation.z, tag_orientation.w]))
 
         # print("ID:",msg.id,"\n Pose:",msg.pose)
