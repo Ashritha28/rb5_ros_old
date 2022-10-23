e0c422a31195bf6360a94b5521b5d5d70caabe56
Sai Ashritha Kandiraju <saiashrithakandiraju@Sais-MacBook-Pro-2.local>
2022-10-23 12:28:30 -0700

    Updated

--- rb5_control/src/localization_node.py
+++ rb5_control/src/localization_node.py
@@ -34,7 +34,7 @@ def tag_callback(msg):
         # print("Rotation matrix using scipy:", r)
         r = tf.transformations.quaternion_matrix([tag_orientation.x, tag_orientation.y, 
         tag_orientation.z, tag_orientation.w])
-        print("Rotation Matrix using tf: \n", r)
+        print("Rotation Matrix using tf: \n", r[:3,:3])
         cTa = (np.concatenate((np.concatenate((r, position), axis=1),np.array([0,0,0,1])),axis=0))
         print("cTa: \n",cTa)
         aTc = np.linalg.inv(cTa)
